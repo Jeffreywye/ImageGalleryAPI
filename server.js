fda889b4eaa159c77229ecea6e1e25dd6b5528cc
Jeffreywye <jeffreywye7@gmail.com>
2020-09-28 06:21:54 -0400

    Added cloudinary files and set-up code to server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const app = express();
+const { cloudinary } = require('./utils/cloudinary');
+var cors = require("cors")
+
+app.use(express.json({limit:"50mb"}));
+app.use(express.urlencoded({limit:"50mb", extended:true}));
+
+app.get("/api/images", cors(),(req, res) => {
+
+});
+
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () =>{
+    console.log(`listening on ${PORT}`)
+});
