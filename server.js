f42f975df3fe0ccbd188021477d16990446801b3
Jeffreywye <jeffreywye7@gmail.com>
2020-10-03 06:34:17 -0400

    added post method

--- server.js
+++ server.js
@@ -10,6 +10,21 @@ app.get("/api/images", cors(),(req, res) => {
 
 });
 
+app.post("/api/upload", cors(), (req,res)=>{
+    try{
+        const fileStr = req.body.data
+        const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
+            upload_preset: 'Image-Gallery-API'
+        });
+        console.log(uploadedResponse);
+        res.json({msg: "File Uploaded"});
+    }
+    catch (error){
+        console.log(error);
+        res.status(500).json({error : "upload failed"})
+    }
+});
+
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () =>{
