2a383b1c8899a57a9eb09104b0251e22e18fb07b
Jeffreywye <jeffreywye7@gmail.com>
2020-10-06 07:30:30 -0400

    added code for image ger request

--- server.js
+++ server.js
@@ -6,17 +6,27 @@ var cors = require("cors")
 app.use(express.json({limit:"50mb"}));
 app.use(express.urlencoded({limit:"50mb", extended:true}));
 
-app.get("/api/images", cors(),(req, res) => {
-
+app.get("/api/images", cors(), async (req, res) => {
+    try{
+        const {resources} = await cloudinary.search.expression('folder: Image-Gallery-API').sort_by('public_id','desc').max_results(30).execute();
+        const publicIds = resources.map((file,index,arr)=>{
+            return file.public_id;
+        });
+        res.send(publicIds);
+    }
+    catch(error){
+        res.status(500).json({error : "failed to obtain images"})
+    }
 });
 
-app.post("/api/upload", cors(), (req,res)=>{
+app.post("/api/upload", cors(), async (req,res)=>{
     try{
         const fileStr = req.body.data
         const uploadedResponse = await cloudinary.uploader.upload(fileStr, {
             upload_preset: 'Image-Gallery-API'
         });
         console.log(uploadedResponse);
+        console.log("Uploaded");
         res.json({msg: "File Uploaded"});
     }
     catch (error){
